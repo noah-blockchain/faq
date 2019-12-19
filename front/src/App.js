71217bef4dd8b3add89d2f93ca520c67ab693083
c137zod <c137zod@gmail.com>
2019-12-19 18:17:45 +0300

    fixed styles

--- front/src/App.js
+++ front/src/App.js
@@ -1,36 +1,9 @@
 import React from "react";
-import { createGlobalStyle } from 'styled-components';
 import { FAQquery } from "./containers";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    @import url('https://fonts.googleapis.com/css?family=Lato:400,700&display=swap');
-    margin: 0;
-    padding: 0;
-    font-family: 'Lato', sans-serif;
-    margin: 0;
-    padding: 0;
-    min-height: 400px;
-    @media (min-width: 640px) {
-      min-width:100px;
-    }
-  }
-   blockquote {
-    border-left: 5px solid #eee;
-    color: #666;
-    font-family: Hoefler Text,Georgia,serif;
-    font-style: italic;
-    margin: 16px 0;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    padding: 10px 20px;
-  }
-`;
 
 export const App = props => {
   return (
       <div>
-        <GlobalStyle />
         <FAQquery />
       </div>
     );
--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createGlobalStyle } from 'styled-components/macro';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Lato:400,700&display=swap');
+    margin: 0;
+    padding: 0;
+    font-family: 'Lato', sans-serif;
+    margin: 0;
+    padding: 0;
+    min-height: 400px;
+    @media (min-width: 640px) {
+      min-width:100px;
+    }
+  }
+   blockquote {
+    border-left: 5px solid #eee;
+    color: #666;
+    font-family: Hoefler Text,Georgia,serif;
+    font-style: italic;
+    margin: 16px 0;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    padding: 10px 20px;
+  }
+`;
+
+ReactDOM.render(
+    <div>
+    <GlobalStyle />
+    <App />
+  </div>, 
+document.getElementById("root"));
